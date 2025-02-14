831baebe2b749217a61ed4521269660815ab22af
SalemBajjali <salem.bajjali@gmail.com>
2025-02-14 17:53:15 -0600

    Add unit tests for allele profile translation functionality

---
+++ test/test_alleleprofile_translation.py
@@ -0,0 +1,115 @@
+import pytest
+from profiles.alleleprofile import AlleleProfile
+from moldeftranslator.allele_translator import VrsFhirAlleleTranslation
+
+@pytest.fixture
+def example():
+    return {
+    "resourceType" : "MolecularDefinition",
+    "id" : "demo-example-hgvs-substitition",
+    "meta" : {
+      "profile" : ["http://hl7.org/fhir/StructureDefinition/allelesliced"]
+    },
+    "moleculeType" : {
+      "coding" : [{
+        "system" : "http://hl7.org/fhir/sequence-type",
+        "code" : "dna",
+        "display" : "DNA Sequence"
+      }]
+    },
+    "location" : [
+        {
+      "sequenceLocation" : {
+        "sequenceContext" : {
+          "reference" : "MolecularDefinition/example-sequence-nc000002-url",
+          "type" : "MolecularDefinition",
+          # Example needs to contain the reference sequence for translation 
+          "display" : "NC_000002.12"
+        },
+        "coordinateInterval" : {
+          "coordinateSystem" : {
+            "system" : {
+              "coding" : [{
+                "system" : "http://loinc.org",
+                "code" : "LA30100-4",
+                # Example needs to contain the systems coordinate for translation 
+                "display" : "0-based interval counting" 
+              }],
+              "text" : "0-based interval counting"
+            }
+          },
+          # Example needs to contain the startQuantity for translation 
+          "startQuantity" : {
+            "value" : 27453448
+          },
+          # Example needs to contain the endQuantity for translation 
+          "endQuantity" : {
+            "value" : 27453449
+          }
+        }
+      }
+    }
+    ],
+    "representation" : [{
+      "focus" : {
+        "coding" : [{
+          "system" : "http://hl7.org/fhir/moleculardefinition-focus",
+          "code" : "allele-state",
+          "display" : "Allele State"
+        }]
+      },
+      "literal" : {
+        # Example needs to contain the literal value for translation
+        "value" : "T"
+      }
+    }]
+  }
+
+@pytest.fixture
+def allele_translator():
+    return VrsFhirAlleleTranslation()
+
+@pytest.fixture
+def allele_profile(example):
+    return AlleleProfile(**example)
+
+@pytest.fixture
+def vrs_expected_outputs():
+    return {
+        True: {  # Normalized output
+            "_id": "ga4gh:VA.fXvhngewkkyVwzEeSJRr5tro8Jcol6Q-",
+            "type": "Allele",
+            "location": {
+                "_id": "ga4gh:VSL.nLMbYalHO4OEI2axqkyTMCQxrH98UpDN",
+                "type": "SequenceLocation",
+                "sequence_id": "ga4gh:SQ.pnAqCRBrTsUoBghSD1yp_jXWSmlbdh4g",
+                "interval": {
+                    "type": "SequenceInterval",
+                    "start": {"type": "Number", "value": 27453448},
+                    "end": {"type": "Number", "value": 27453449}
+                }
+            },
+            "state": {"type": "LiteralSequenceExpression", "sequence": "T"}
+        },
+        False: {  # Unnormalized output
+            "type": "Allele",
+            "location": {
+                "type": "SequenceLocation",
+                "sequence_id": "refseq:NC_000002.12",
+                "interval": {
+                    "type": "SequenceInterval",
+                    "start": {"type": "Number", "value": 27453448},
+                    "end": {"type": "Number", "value": 27453449}
+                }
+            },
+            "state": {"type": "LiteralSequenceExpression", "sequence": "T"}
+        }
+    }
+
+@pytest.mark.parametrize("normalize", [True, False])
+def test_translate_allele_profile(allele_translator, allele_profile, vrs_expected_outputs, normalize):
+    output_dict = allele_translator.translate_allele_profile_to_vrs_allele(
+        allele_profile, normalize=normalize
+    ).as_dict()
+
+    assert output_dict == vrs_expected_outputs[normalize]
