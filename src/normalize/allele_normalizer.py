3cf118425014a0b36ebe2d09ea1bef824a8575dc
SalemBajjali <salem.bajjali@gmail.com>
2025-03-31 13:36:54 -0500

    refactor(AlleleNormalizer): simplify allele ID assignment and remove unnecessary sequence ID translation

--- src/normalize/allele_normalizer.py
+++ src/normalize/allele_normalizer.py
@@ -1,7 +1,6 @@
 # NOTE: This is just a temporary class for now.
 from ga4gh.core import ga4gh_identify
 from ga4gh.vrs import normalize as vrs_normalize
-
 from api.seqrepo_api import SeqRepoAPI
 
 
@@ -22,16 +21,11 @@ class AlleleNormalizer:
             models.Allele: The normalized VRS allele with GA4GH identifiers.
 
         """
-        # Translating the sequence id to ga4gh format
-        seq_id = self.dp.translate_sequence_identifier(
-            allele.location.sequence_id._value, "ga4gh"
-        )[0]
-        allele.location.sequence_id = seq_id
+
         # Using the ga4gh normalize function to normalize the allele. (Coming form biocommons.normalize())
         allele = vrs_normalize(allele, self.dp)
         # Setting the allele id to a  GA4GH digest-based id for the object, as a CURIE
-        allele._id = ga4gh_identify(allele)
+        allele.id = ga4gh_identify(allele)
         # Setting the location id to a GA4GH digest-based id for the object, as a CURIE
-        allele.location._id = ga4gh_identify(allele.location)
+        allele.location.id = ga4gh_identify(allele.location)
         return allele
-
