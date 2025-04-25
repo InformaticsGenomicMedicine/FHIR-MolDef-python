dfeaaf3a7913a3d594af351b9fa6e461b28a6633
SalemBajjali <salem.bajjali@gmail.com>
2025-04-25 14:44:17 -0500

    fix: update allele profile translation test to support vrs 2.0

--- test/test_alleleprofile_translation.py
+++ test/test_alleleprofile_translation.py
@@ -7,136 +7,138 @@ from profiles.allele import Allele as FhirAllele
 @pytest.fixture
 def example():
     return {
-    "resourceType": "MolecularDefinition",
-    "contained": [
-        {
-            "resourceType": "MolecularDefinition",
-            "moleculeType": {
-                "coding": [
-                    {
-                        "system": "http://hl7.org/fhir/sequence-type",
-                        "code": "dna",
-                        "display": "DNA Sequence"
-                    }
-                ]
-            },
-            "representation": [
-                {
-                    "code": [
+        "resourceType": "MolecularDefinition",
+        "contained": [
+            {
+                "resourceType": "MolecularDefinition",
+                "moleculeType": {
+                    "coding": [
                         {
-                            "coding": [
-                                {
-                                    "system": "http://www.ncbi.nlm.nih.gov/refseq",
-                                    "code": "NC_000002.12"
-                                }
-                            ]
+                            "system": "http://hl7.org/fhir/sequence-type",
+                            "code": "dna",
+                            "display": "DNA Sequence",
                         }
                     ]
+                },
+                "representation": [
+                    {
+                        "code": [
+                            {
+                                "coding": [
+                                    {
+                                        "system": "http://www.ncbi.nlm.nih.gov/refseq",
+                                        "code": "NC_000002.12",
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ],
+            }
+        ],
+        "moleculeType": {
+            "coding": [
+                {
+                    "system": "http://hl7.org/fhir/sequence-type",
+                    "code": "dna",
+                    "display": "DNA Sequence",
                 }
             ]
-        }
-    ],
-    "moleculeType": {
-        "coding": [
+        },
+        "location": [
             {
-                "system": "http://hl7.org/fhir/sequence-type",
-                "code": "dna",
-                "display": "DNA Sequence"
-            }
-        ]
-    },
-    "location": [
-        {
-            "sequenceLocation": {
-                "sequenceContext": {
-                    "reference": "MolecularDefinition/example-sequence-nc000002-url",
-                    "type": "MolecularDefinition",
-                },
-                "coordinateInterval": {
-                    "coordinateSystem": {
-                        "system": {
-                            "coding": [
-                                {
-                                    "system": "http://loinc.org",
-                                    "code": "LA30100-4",
-                                    "display": "0-based interval counting"
-                                }
-                            ]
-                        }
+                "sequenceLocation": {
+                    "sequenceContext": {
+                        "reference": "MolecularDefinition/example-sequence-nc000002-url",
+                        "type": "MolecularDefinition",
                     },
-                    "startQuantity": {
-                        "value": 27453448
+                    "coordinateInterval": {
+                        "coordinateSystem": {
+                            "system": {
+                                "coding": [
+                                    {
+                                        "system": "http://loinc.org",
+                                        "code": "LA30100-4",
+                                        "display": "0-based interval counting",
+                                    }
+                                ]
+                            }
+                        },
+                        "startQuantity": {"value": 27453448},
+                        "endQuantity": {"value": 27453449},
                     },
-                    "endQuantity": {
-                        "value": 27453449
-                    }
                 }
             }
-        }
-    ],
-    "representation": [
-        {
-            "focus": {
-                "coding": [
-                    {
-                        "system": "http://hl7.org/fhir/moleculardefinition-focus",
-                        "code": "allele-state",
-                        "display": "Allele State"
-                    }
-                ]
-            },
-            "literal": {
-                "value": "T"
+        ],
+        "representation": [
+            {
+                "focus": {
+                    "coding": [
+                        {
+                            "system": "http://hl7.org/fhir/moleculardefinition-focus",
+                            "code": "allele-state",
+                            "display": "Allele State",
+                        }
+                    ]
+                },
+                "literal": {"value": "T"},
             }
-        }
-    ]
-}
+        ],
+    }
+
 
 @pytest.fixture
 def allele_translator():
     return VrsFhirAlleleTranslation()
 
+
 @pytest.fixture
 def allele_profile(example):
     return FhirAllele(**example)
 
+
 @pytest.fixture
 def vrs_expected_outputs():
     return {
-        True: {  # Normalized output
-            "_id": "ga4gh:VA.fXvhngewkkyVwzEeSJRr5tro8Jcol6Q-",
+        True: {
+            "id": "ga4gh:VA.xfKU4c8mG_yegL5ZOL26JDiznySNkoMl",
             "type": "Allele",
+            "digest": "xfKU4c8mG_yegL5ZOL26JDiznySNkoMl",
             "location": {
-                "_id": "ga4gh:VSL.nLMbYalHO4OEI2axqkyTMCQxrH98UpDN",
+                "id": "ga4gh:SL.y0ckc1_lhMYKnh0f6FAEoEpgHyfX13OW",
                 "type": "SequenceLocation",
-                "sequence_id": "ga4gh:SQ.pnAqCRBrTsUoBghSD1yp_jXWSmlbdh4g",
-                "interval": {
-                    "type": "SequenceInterval",
-                    "start": {"type": "Number", "value": 27453448},
-                    "end": {"type": "Number", "value": 27453449}
-                }
+                "digest": "y0ckc1_lhMYKnh0f6FAEoEpgHyfX13OW",
+                "sequenceReference": {
+                    "type": "SequenceReference",
+                    "refgetAccession": "SQ.pnAqCRBrTsUoBghSD1yp_jXWSmlbdh4g",
+                },
+                "start": 27453448,
+                "end": 27453449,
             },
-            "state": {"type": "LiteralSequenceExpression", "sequence": "T"}
+            "state": {"type": "LiteralSequenceExpression", "sequence": "T"},
         },
-        False: {  # Unnormalized output
+        False: {
             "type": "Allele",
             "location": {
                 "type": "SequenceLocation",
-                "sequence_id": "refseq:NC_000002.12",
-                "interval": {
-                    "type": "SequenceInterval",
-                    "start": {"type": "Number", "value": 27453448},
-                    "end": {"type": "Number", "value": 27453449}
-                }
+                "sequenceReference": {
+                    "type": "SequenceReference",
+                    "refgetAccession": "SQ.pnAqCRBrTsUoBghSD1yp_jXWSmlbdh4g",
+                },
+                "start": 27453448,
+                "end": 27453449,
             },
-            "state": {"type": "LiteralSequenceExpression", "sequence": "T"}
-        }
+            "state": {"type": "LiteralSequenceExpression", "sequence": "T"},
+        },
     }
 
+
 @pytest.mark.parametrize("normalize", [True, False])
-def test_translate_allele_profile(allele_translator, allele_profile, vrs_expected_outputs, normalize):
-    output_dict = allele_translator.translate_allele_profile_to_vrs_allele(
+def test_translate_allele_profile(
+    allele_translator, allele_profile, vrs_expected_outputs, normalize
+):
+    output_dict = allele_translator.allele_profile_to_vrs_allele(
         allele_profile, normalize=normalize
-    ).as_dict()
+    ).model_dump(exclude_none=True)
 
     assert output_dict == vrs_expected_outputs[normalize]
--- test/test_vrs_to_alleleprofile_translation.py
+++ test/test_vrs_to_alleleprofile_translation.py
@@ -1,125 +1,127 @@
 from decimal import Decimal
 
 import pytest
-from ga4gh.vrs import models
+from ga4gh.vrs.models import Allele
 
 from translators.allele_translator import VrsFhirAlleleTranslation
 
 
 @pytest.fixture
 def example():
-    return {  # Normalized output
-            "_id": "ga4gh:VA.fXvhngewkkyVwzEeSJRr5tro8Jcol6Q-",
-            "type": "Allele",
-            "location": {
-                "_id": "ga4gh:VSL.nLMbYalHO4OEI2axqkyTMCQxrH98UpDN",
-                "type": "SequenceLocation",
-                "sequence_id": "ga4gh:SQ.pnAqCRBrTsUoBghSD1yp_jXWSmlbdh4g",
-                "interval": {
-                    "type": "SequenceInterval",
-                    "start": {"type": "Number", "value": 27453448},
-                    "end": {"type": "Number", "value": 27453449}
-                }
+    return {
+        "id": "ga4gh:VA.3edM6TTGAmx8DnPV-uzA6IYlAfatAP2s",
+        "type": "Allele",
+        "digest": "3edM6TTGAmx8DnPV-uzA6IYlAfatAP2s",
+        "location": {
+            "id": "ga4gh:SL.OUMCiUkn_AGlFuFCFTdfppig932_HV2k",
+            "type": "SequenceLocation",
+            "digest": "OUMCiUkn_AGlFuFCFTdfppig932_HV2k",
+            "sequenceReference": {
+                "type": "SequenceReference",
+                "refgetAccession": "SQ.Ya6Rs7DHhDeg7YaOSg1EoNi3U_nQ9SvO",
             },
-            "state": {"type": "LiteralSequenceExpression", "sequence": "T"}
-        }
+            "start": 113901365,
+            "end": 113901365,
+        },
+        "state": {"type": "LiteralSequenceExpression", "sequence": "ATA"},
+    }
 
-@pytest.fixturex
+
+@pytest.fixture
 def allele_translator():
     return VrsFhirAlleleTranslation()
 
 @pytest.fixture
 def vrs_allele(example):
-    return models.Allele(**example)
+    return Allele(**example)
 
 @pytest.fixture
 def alleleprofile_expected_outputs():
     return {
-    "resourceType": "MolecularDefinition",
-    "contained": [
-        {
-            "resourceType": "MolecularDefinition",
-            "moleculeType": {
-                "coding": [
-                    {
-                        "system": "http://hl7.org/fhir/sequence-type",
-                        "code": "dna",
-                        "display": "DNA Sequence"
-                    }
-                ]
-            },
-            "representation": [
-                {
-                    "code": [
+        "resourceType": "MolecularDefinition",
+        "contained": [
+            {
+                "resourceType": "MolecularDefinition",
+                "id": "ref-to-nc000001",
+                "moleculeType": {
+                    "coding": [
                         {
-                            "coding": [
-                                {
-                                    "system": "http://www.ncbi.nlm.nih.gov/refseq",
-                                    "code": "NC_000002.12"
-                                }
-                            ]
+                            "system": "http://hl7.org/fhir/sequence-type",
+                            "code": "dna",
+                            "display": "DNA Sequence",
                         }
                     ]
+                },
+                "representation": [
+                    {
+                        "code": [
+                            {
+                                "coding": [
+                                    {
+                                        "system": "http://www.ncbi.nlm.nih.gov/refseq",
+                                        "code": "NC_000001.11",
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ],
+            }
+        ],
+        "moleculeType": {
+            "coding": [
+                {
+                    "system": "http://hl7.org/fhir/sequence-type",
+                    "code": "dna",
+                    "display": "DNA Sequence",
                 }
             ]
-        }
-    ],
-    "moleculeType": {
-        "coding": [
+        },
+        "location": [
             {
-                "system": "http://hl7.org/fhir/sequence-type",
-                "code": "dna",
-                "display": "DNA Sequence"
-            }
-        ]
-    },
-    "location": [
-        {
-            "sequenceLocation": {
-                "sequenceContext": {
-                    "reference": "MolecularDefinition/example-sequence-nc000002-url",
-                    "type": "MolecularDefinition",
-                },
-                "coordinateInterval": {
-                    "coordinateSystem": {
-                        "system": {
-                            "coding": [
-                                {
-                                    "system": "http://loinc.org",
-                                    "code": "LA30100-4",
-                                    "display": "0-based interval counting"
-                                }
-                            ]
-                        }
+                "sequenceLocation": {
+                    "sequenceContext": {
+                        "reference": "#ref-to-nc000001",
+                        "type": "MolecularDefinition",
                     },
-                    "startQuantity": {
-                        "value": Decimal(27453448)
+                    "coordinateInterval": {
+                        "coordinateSystem": {
+                            "system": {
+                                "coding": [
+                                    {
+                                        "system": "http://loinc.org",
+                                        "code": "LA30100-4",
+                                        "display": "0-based interval counting",
+                                    }
+                                ]
+                            }
+                        },
+                        "startQuantity": {"value": Decimal(113901365)},
+                        "endQuantity": {"value": Decimal(113901365)},
                     },
-                    "endQuantity": {
-                        "value": Decimal(27453449)
-                    }
                 }
             }
-        }
-    ],
-    "representation": [
-        {
-            "focus": {
-                "coding": [
-                    {
-                        "system": "http://hl7.org/fhir/moleculardefinition-focus",
-                        "code": "allele-state",
-                        "display": "Allele State"
-                    }
-                ]
-            },
-            "literal": {
-                "value": "T"
+        ],
+        "representation": [
+            {
+                "focus": {
+                    "coding": [
+                        {
+                            "system": "http://hl7.org/fhir/moleculardefinition-focus",
+                            "code": "allele-state",
+                        }
+                    ]
+                },
+                "literal": {"value": "ATA"},
             }
-        }
-    ]
-}
+        ],
+    }
 
-def test_translate_vrs_to_alleleprofile(allele_translator, vrs_allele, alleleprofile_expected_outputs):
-    output_dict = allele_translator.vrs_allele_to_allele_profile(vrs_allele).model_dump()
+
+def test_translate_vrs_to_alleleprofile(
+    allele_translator, vrs_allele, alleleprofile_expected_outputs
+):
+    output_dict = allele_translator.vrs_allele_to_allele_profile(
+        vrs_allele
+    ).model_dump()
     assert output_dict == alleleprofile_expected_outputs
