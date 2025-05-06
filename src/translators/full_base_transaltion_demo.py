9809e154cb688c88fbec4287d68fd393eaafff6c
SalemBajjali <salem.bajjali@gmail.com>
2025-05-05 21:37:31 -0500

    feat: enhance map_extension method to handle multiple extensions and improve extension mapping logic

--- src/translators/full_base_transaltion_demo.py
+++ src/translators/full_base_transaltion_demo.py
@@ -8,7 +8,9 @@ from fhir.resources.identifier import Identifier
 from profiles.allele import Allele
 from fhir.resources.extension import Extension
 
-# from resources.moleculardefinition import MolecularDefinition
+from resources.moleculardefinition import MolecularDefinition,MolecularDefinitionRepresentation
+from fhir.resources.codeableconcept import CodeableConcept
+from fhir.resources.coding import Coding
 
 class FullVRSAlleleTranslator:
     def __init__(self, vrs_allele):
@@ -36,59 +38,72 @@ class FullVRSAlleleTranslator:
     def map_description(self):
         return self.vrs_allele.name or None
     
+    def map_extension(self, ext_obj=None, source=None):
+        # Handle top-level call (get all extensions from the source object)
+        if ext_obj is None:
+            source = source or self.vrs_allele
 
-    def map_extension(self):
-        #TODO: figure out what mulitple extensions in a VRS allele object would look like
-        ext_obj = self.vrs_allele.extension[0] #This is inputing only one extesnsion
+            if not getattr(source, "extensions", None):
+                return None
 
+            return [self.map_extension(ext, source=source) for ext in source.extensions]
+
+        # Handle a single extension object
         extension = Extension(
             id=ext_obj.id,
             url=ext_obj.name,
         )
 
-        value = ext_obj.value 
+        value = ext_obj.value
 
         type_to_attr = {
             str: 'valueString',
             bool: 'valueBoolean',
             dict: 'valueCode',
-            float: 'valueDecimal'
-            #TODO: Need to figure out what list in extensions.values wold mapp to in FHIR values[x]
-            }
-        
+            float: 'valueDecimal',
+            int: 'valueInteger'
+        }
+
         for expected_type, attr_name in type_to_attr.items():
             if isinstance(value, expected_type):
-                setattr(extension, attr_name, value) 
+                setattr(extension, attr_name, value)
                 break
 
+        # Handle sub-extensions
         sub_extensions = []
+
         if ext_obj.description:
             sub_extensions.append(
                 Extension(
-                    url= "description",
+                    url="description",
                     valueString=ext_obj.description
                 )
             )
 
         if ext_obj.extensions:
             for nested in ext_obj.extensions:
-                sub_extensions.append(self.build_extension(nested))
-        
+                sub_extensions.append(self.map_extension(nested, source=ext_obj))
+
         if sub_extensions:
             extension.extension = sub_extensions
 
-            return extension
-    #TODO: Continue development expressions
+        return extension
+
+
+    
+    #TODO: For right now use MolecularDefinition becuase it has much less restirctions then Allele
     def translate(self):
-        return Allele(
+        return MolecularDefinition( 
             identifier=self.map_identifiers(),
             description=self.map_description(),
             extension=self.map_extension(),
-        )
-    
-if __name__ == "__main__":
 
-    # vrs_allele_instance =  #TODO: input data here whenc created
-    translator = FullVRSAlleleTranslator(vrs_allele_instance)
-    fhir_allele = translator.translate()
-    print(fhir_allele)
+
+        )
+
+# if __name__ == "__main__":
+
+#     # vrs_allele_instance =  #TODO: input data here whenc created
+#     translator = FullVRSAlleleTranslator(vrs_allele_instance)
+#     fhir_allele = translator.translate()
+#     print(fhir_allele)
