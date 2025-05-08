1834d57bfbe36b5ebe8cbc4863cef63f4b4af872
SalemBajjali <salem.bajjali@gmail.com>
2025-05-08 14:18:10 -0500

    feat: enhance FullVRSAlleleTranslator with location and representation mapping methods

--- src/translators/full_base_transaltion_demo.py
+++ src/translators/full_base_transaltion_demo.py
@@ -1,6 +1,7 @@
 from fhir.resources.identifier import Identifier
 from profiles.allele import Allele
 from fhir.resources.extension import Extension
+from fhir.resources.quantity import Quantity
 
 from resources.moleculardefinition import MolecularDefinition,MolecularDefinitionRepresentation
 from fhir.resources.codeableconcept import CodeableConcept
@@ -8,6 +9,18 @@ from fhir.resources.coding import Coding
 from translators.allele_utils import (
     is_valid_vrs_allele,
 )
+from fhir.resources.reference import Reference
+
+from resources.moleculardefinition import (
+    MolecularDefinitionLocation,
+    MolecularDefinitionLocationSequenceLocation,
+    MolecularDefinitionLocationSequenceLocationCoordinateInterval,
+    MolecularDefinitionLocationSequenceLocationCoordinateIntervalCoordinateSystem,
+    MolecularDefinitionRepresentation,
+    MolecularDefinitionRepresentationLiteral,
+)
+from profiles.sequence import Sequence as FhirSequence
+
 class FullVRSAlleleTranslator:
     def __init__(self, vrs_allele):
         self.vrs_allele = vrs_allele
@@ -15,7 +28,17 @@ class FullVRSAlleleTranslator:
     def validate_vrs_allele(self):
         #This is where VRS type is getting checked to make sure its a vrs allele
         return is_valid_vrs_allele(self.vrs_allele)
+    
+    #TODO: For right now use MolecularDefinition becuase it has much less restirctions then Allele
+    def translate(self):
+        return MolecularDefinition( 
+            identifier=self.map_identifiers(),
+            description=self.map_description(),
+            extension=self.map_extensions(),
+            representation=[self.create_rep()]
+        )
 
+    # ─── Identifier & Description Mappers ───
     def map_identifiers(self):
         identifiers = []
 
@@ -27,8 +50,8 @@ class FullVRSAlleleTranslator:
                 Identifier(value=alias) for alias in self.vrs_allele.aliases
             ])
 
-        if self.vrs_allele.description:
-            identifiers.append(Identifier(value=self.vrs_allele.description))
+        if self.vrs_allele.name:
+            identifiers.append(Identifier(value=self.vrs_allele.name))
 
         if self.vrs_allele.digest:
             identifiers.append(Identifier(system=self.vrs_allele.digest))
@@ -36,8 +59,9 @@ class FullVRSAlleleTranslator:
         return identifiers or None
 
     def map_description(self):
-        return self.vrs_allele.name or None
+        return self.vrs_allele.description or None
     
+    # ─── Extension Mappers ───
     def map_extensions(self, source=None):
         source = source or self.vrs_allele
         vrs_exts = getattr(source, "extensions", None)
@@ -88,6 +112,109 @@ class FullVRSAlleleTranslator:
                 setattr(extension, attr_name, value)
                 return
         raise ValueError("Only support extension values of types: str, bool, dict, float.")
+    
+    # ─── Location Mappers  ───
+
+    def map_location_extensions(self):
+        location_extensions = []
+
+        if self.vrs_allele.location.name:
+            name_ext = Extension(
+                url="http://example.org/fhir/StructureDefinition/sequence-location-name",
+                valueString=self.vrs_allele.location.name
+            )
+            location_extensions.append(name_ext)
+
+        if self.vrs_allele.location.description:
+            description_ext = Extension(
+                url="http://example.org/fhir/StructureDefinition/sequence-location-description",
+                valueString=self.vrs_allele.location.description
+            )
+            location_extensions.append(description_ext)
+
+        for alias in self.vrs_allele.location.aliases or []:
+            alias_ext = Extension(
+                url="http://example.org/fhir/StructureDefinition/sequence-location-alias",
+                valueString=alias
+            )
+            location_extensions.append(alias_ext)
+
+        for exp in self.vrs_allele.location.extensions or []:
+            exts = self.map_extensions(source=exp)
+            if exts:
+                location_extensions.extend(exts)
+
+        if self.vrs_allele.location.digest:
+            digest_ext = Extension(
+                url="http://example.org/fhir/StructureDefinition/sequence-location-digest",
+                valueString=self.vrs_allele.location.digest
+            )
+            location_extensions.append(digest_ext)
+
+        return location_extensions
+
+    def map_coordiante_interval(self):
+        start_quant = Quantity(value=int(self.vrs_allele.location.start))
+        end_quant = Quantity(value=int(self.vrs_allele.location.start))
+        coord_system = CodeableConcept(
+            coding=[
+                {
+                    "system": "http://loinc.org",
+                    "code": "LA30100-4",
+                    "display": "0-based interval counting",
+                }
+            ]
+        )
+        coord_system_fhir = MolecularDefinitionLocationSequenceLocationCoordinateIntervalCoordinateSystem(
+            system=coord_system
+        )
+        coord_interval = MolecularDefinitionLocationSequenceLocationCoordinateInterval(
+            coordinateSystem=coord_system_fhir,
+            startQuantity=start_quant,
+            endQuantity=end_quant,
+        )
+
+        return coord_interval
+
+    #TODO: revisit this: Need to figure out where sequence needs to go in locaiton compared to fhir. because if we place it in sequenceLocaiton.seuqenceContext its a 1..1 cardinality
+    def map_sequence_location(self):
+        representation_sequence = MolecularDefinitionRepresentation(literal=self.vrs_allele.location.sequence)
+
+        sequence_profile = FhirSequence(
+            representation=[representation_sequence],
+        )
+        seq_context = Reference(
+            reference=f"#{sequence_profile.id}", type="MolecularDefinition"
+        )
+        MolecularDefinitionLocationSequenceLocation(
+            SequenceContext = seq_context
+        )
+    
+    #TODO: revisit this: The same as teh above statement. sequenceReference fits est with sequencelocation.seuqenctCtontext but this is a 1..1 cardinality. might need to create a loop to see if one is present then cant transalte the other.
+    def map_sequence_reference_to_sequence_location(self):
+        representation_sequence = MolecularDefinitionRepresentation(literal=self.vrs_allele.location.sequenceReference.sequence)
+        sequence_profile = FhirSequence(
+            id=self.vrs_allele.location.sequenceReference.id,
+            description=self.vrs_allele.location.sequenceReference.discription,
+            # extension=,
+            moleculeType=self.vrs_allele.location.sequenceReference.moleculeType,
+            representation=[representation_sequence],
+        )
+        seq_context = Reference(
+            reference=f"#{sequence_profile.id}", type="MolecularDefinition"
+        )
+        MolecularDefinitionLocationSequenceLocation(
+            SequenceContext = seq_context
+        )
+    
+    #TODO: figure out how to put everything together
+    # def map_location(self):
+        # MolecularDefinitionLocation(
+        #     id=self.vrs_allele.location.id
+        #     extension=self.map_location_extensions()
+        # )
+
+    # ─── Literal Representation Mappers ───
 
     def map_code(self):
         concepts = []
@@ -111,16 +238,54 @@ class FullVRSAlleleTranslator:
 
         return concepts
     
+    #TODO: revisit this, need to include the other values in represntation:
     def create_rep(self):
         
         rep = MolecularDefinitionRepresentation(code=self.map_code())
         return rep 
+    
+    def map_representation_extensions(self):
+        representation_extensions = []
 
-    #TODO: For right now use MolecularDefinition becuase it has much less restirctions then Allele
-    def translate(self):
-        return MolecularDefinition( 
-            identifier=self.map_identifiers(),
-            description=self.map_description(),
-            extension=self.map_extensions(),
-            representation=[self.create_rep()]
+        if self.vrs_allele.state.name:
+            name_ext = Extension(
+                url="http://example.org/fhir/LiteralRepresntation/name",
+                valueString=self.vrs_allele.state.name
+            )
+            representation_extensions.append(name_ext)
+
+        if self.vrs_allele.state.description:
+            description_ext = Extension(
+                url="http://example.org/fhir/LiteralRepresntation/location-description",
+                valueString=self.vrs_allele.state.description
+            )
+            representation_extensions.append(description_ext)
+
+        for alias in self.vrs_allele.state.aliases or []:
+            alias_ext = Extension(
+                url="http://example.org/fhir/LiteralRepresntation/location-alias",
+                valueString=alias
+            )
+            representation_extensions.append(alias_ext)
+
+        for exp in self.vrs_allele.state.expressions or []:
+            exts = self.map_extensions(source=exp)
+            if exts:
+                representation_extensions.extend(exts)
+
+        if self.vrs_allele.state.digest:
+            digest_ext = Extension(
+                url="http://example.org/fhir/LiteralRepresntation/location-digest",
+                valueString=self.vrs_allele.state.digest
+            )
+            representation_extensions.append(digest_ext)
+
+        return representation_extensions
+        
+    def map_literal_representation(self):
+        MolecularDefinitionRepresentationLiteral(
+            id=self.vrs_allele.state.id, 
+            #TODO: figure out how to have extension for things that are repeated
+            # extension=self.map_representation_extensions()
+            value=self.vrs_allele.state.sequence
         )
