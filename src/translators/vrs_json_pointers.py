228bc96600c2717638ae0b5d8ecf46add490c181
SalemBajjali <salem.bajjali@gmail.com>
2025-05-28 13:06:26 -0500

    feat: refactor identifier definitions to use a build function for improved maintainability

--- src/translators/vrs_json_pointers.py
+++ src/translators/vrs_json_pointers.py
@@ -1,58 +1,29 @@
 #TODO: Follow up with: https://github.com/ga4gh/vrs/issues/652
-allele_identifiers= {
-    'id': 'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/Allele#properties/id',
-    'name': 'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/Allele#properties/name',
-    'aliases': 'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/Allele#properties/aliases',
-    'digest': 'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/Allele#properties/digest',
-    'location':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/Allele#properties/location',
-    'state': 'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/Allele#properties/state',
-    }
+BASE_URL = 'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/'
 
-literal_sequence_expression_identifiers = {
-    'id': 'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/LiteralSequenceExpression#properties/id',
-    'name':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/LiteralSequenceExpression#properties/name',
-    'description':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/LiteralSequenceExpression#properties/description',
-    'aliases':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/LiteralSequenceExpression#properties/aliases',
-    'extensions':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/LiteralSequenceExpression#properties/extensions',
-    'sequence': 'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/LiteralSequenceExpression#properties/sequence',
-}
+def build_identifier(entity, fields):
+    return {field: f"{BASE_URL}{entity}#properties/{field}" for field in fields}
 
-expression_identifiers = {
-    'id': 'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/Expression#properties/id',
-    'extensions':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/Expression#properties/extensions',
-    'syntax':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/Expression#properties/syntax',
-    'value':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/Expression#properties/value',
-    'syntax_version':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/Expression#properties/syntax_version',
-}
-
-sequence_location_identifiers = {
-    'id': 'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceLocation#properties/id',
-    'name':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceLocation#properties/name',
-    'description':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceLocation#properties/description',
-    'aliases':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceLocation#properties/aliases',
-    'extensions':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceLocation#properties/extensions',
-    'digest':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceLocation#properties/digest',
-    'sequenceReference':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceLocation#properties/sequenceReference',
-    'start':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceLocation#properties/start',
-    'end':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceLocation#properties/end',
-    'sequence':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceLocation#properties/sequence',
-
-}
-
-sequence_reference_identifiers = {
-    'id': 'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceReference#properties/id',
-    'name':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceReference#properties/name',
-    'description':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceReference#properties/description',
-    'aliases':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceReference#properties/aliases',
-    'extensions':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceReference#properties/extensions',
-    'refgetAccession':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceReference#properties/refgetAccession',
-    'residueAlphabet':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceReference#properties/residueAlphabet',
-    'sequence':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceReference#properties/sequence',
-    'moleculeType':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceReference#properties/moleculeType',
-    'circular':'https://w3id.org/ga4gh/schema/vrs/2.0.1/json/SequenceReference#properties/circular',
-
-}
-
+allele_identifiers = build_identifier(
+    entity='Allele',
+    fields=['id', 'name', 'aliases', 'digest', 'location', 'state']
+)
+literal_sequence_expression_identifiers = build_identifier(
+    entity='LiteralSequenceExpression',
+    fields=['id', 'name', 'description', 'aliases', 'extensions', 'sequence']
+)
+expression_identifiers = build_identifier(
+    entity='Expression',
+    fields=['id', 'extensions', 'syntax', 'value', 'syntax_version']
+)
+sequence_location_identifiers = build_identifier(
+    entity='SequenceLocation',
+    fields=['id', 'name', 'description', 'aliases', 'extensions', 'digest', 'sequenceReference', 'start', 'end', 'sequence']
+)
+sequence_reference_identifiers = build_identifier(
+    entity='SequenceReference',
+    fields=['id', 'name', 'description', 'aliases', 'extensions', 'refgetAccession', 'residueAlphabet', 'sequence', 'moleculeType', 'circular']
+)
 sequence_string_identifier = {
-    'id': "https://w3id.org/ga4gh/schema/vrs/2.0.1/json/sequenceString"
+    'id': f"{BASE_URL}sequenceString"
 }
