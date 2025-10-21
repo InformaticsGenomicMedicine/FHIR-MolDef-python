fad42f82f32bc7662e653195fe812671106f3c3d
SalemBajjali <salem.bajjali@gmail.com>
2025-10-21 14:45:08 -0500

    feat: add detailed docstrings for SPDI and HGVS parsing methods in VariationTranslation class

--- src/translators/vartiation_translator.py
+++ src/translators/vartiation_translator.py
@@ -11,8 +11,7 @@ from fhir.resources.reference import Reference
 # connect to the UTA (Universal Transcript Archive) database via hgvs.dataproviders.uta.
 # UTA is required for HGVS normalization and transcript/genomic coordinate mapping,
 # because those operations depend on transcript alignment data stored in UTA.
-from ga4gh.vrs.utils.hgvs_tools import HgvsTools 
-
+from ga4gh.vrs.utils.hgvs_tools import HgvsTools
 
 from api.seqrepo import SeqRepoClient
 from profiles.variation import Variation
@@ -36,10 +35,22 @@ class VariationTranslation:
         self.hgvs_tools = HgvsTools()
 
     def _from_spdi(self,spdi):
+        """Parse an SPDI string and convert it into a FHIR Variation Profile object.
 
+        Args:
+            spdi (str): A valid spdi string. "<sequence_accession>:<position>:<deleted_sequence_or_length>:<inserted_sequence>".
+
+        Raises:
+            TypeError: If the provided `spdi` argument is not a string.
+            ValueError:  If the SPDI string does not contain four colon-separated fields or 
+            cannot be parsed correctly.
+
+        Returns:
+            object: A FHIR Variation Profile object representing the parsed SPDI variant.
+        """
         if not isinstance(spdi,str):
-            raise TypeError(f"SPDI expression must be a string.")
-        
+            raise TypeError("SPDI expression must be a string.")
+
         try:
             seq_acc,pos,del_seq_or_len,ins_seq = spdi.split(":",maxsplit=3)
         except:
@@ -48,7 +59,7 @@ class VariationTranslation:
         seq_acc = str(seq_acc).strip()
 
         start = int(pos)
-        
+
         try:
             del_len = int(del_seq_or_len)
         except ValueError:
@@ -72,7 +83,18 @@ class VariationTranslation:
         return self._create_variation_profile(values)
 
     def _from_hgvs(self,hgvs_expr):
+        """Parse an HGVS string and convert it into a FHIR Variation Profile object  
 
+        Args:
+            hgvs_expr (str): A valid hgvs expression. 
+
+        Raises:
+            ValueError: If the HGVS expression represents an intronic variant, which is not supported.
+            NotImplementedError: If the HGVS edit type is not supported by this implementation.
+
+        Returns:
+            object: A FHIR Variation Profile object representing the parsed HGVS variant.
+        """
         # we need to parse
         sv = self.hgvs_tools.parse(hgvs_expr)
         if not sv:
@@ -108,7 +130,9 @@ class VariationTranslation:
             }
 
         return self._create_variation_profile(values)
-    
+    # TODO: This function was copied from another module. Consider moving it to `allele_utils.py`.
+    # NOTE: The name `allele_utils` might not be ideal, since this function is also used in the
+    #       variation module, which could cause confusion.
     def _refseq_to_fhir_id(self, refseq_accession):
         """Converts a RefSeq accession string to a standardized FHIR ID format.
         This method removes the version suffix (after the dot), strips out underscores,
@@ -121,9 +145,16 @@ class VariationTranslation:
             str: A formatted FHIR-compatible ID (e.g., 'nm001256789').
         """
         return refseq_accession.split(".", 1)[0].replace("_", "").lower()
-    
-    def _create_variation_profile(self,values):
 
+    def _create_variation_profile(self,values):
+        """Create a FHIR Variation resource from parsed variant data (HGVS or SPDI).
+
+        Args:
+            values (dict): A dictionary containing variant attributes. (Refget Accession,Start,End,Reference Sequence, Alternative Sequence)
+
+        Returns:
+            object: A fully populated FHIR Variation object.
+        """
         MOLTYPE_SYSTEM_DEFAULT_VALUE = "http://hl7.org/fhir/uv/molecular-definition-data-types/CodeSystem/molecule-type"
         FOCUS_SYSTEM_DEFAULT_VALUE = "http://hl7.org/fhir/uv/molecular-definition-data-types/CodeSystem/molecular-definition-focus"
 
@@ -155,7 +186,7 @@ class VariationTranslation:
         #     )
 
         fhir_id = self._refseq_to_fhir_id(refseq_accession=values["refget_accession"])
-        
+
         sequence_context = Reference(
             reference=f"#ref-to-{fhir_id}",
             type = "MolecularDefinition",
@@ -166,7 +197,7 @@ class VariationTranslation:
                 system=coord_system_values,
                 # normalizationMethod= normalization_method
             )
-        
+
         start, end = Quantity(value=int(values["start"])),Quantity(value=int(values["end"]))
 
         coord_interval = MolecularDefinitionLocationSequenceLocationCoordinateInterval(
@@ -218,9 +249,20 @@ class VariationTranslation:
             location=[location],
             representation=[ref_state_rep,alt_state_rep]
         )
-    
-    def translate_from(self, var, fmt):
 
+    def translate_from(self, var, fmt):
+        """Translate a variant (HGVS or SPDI) into a FHIR Variation object.
+
+        Args:
+            var (str): A variant expression in HGVS or SPDI format.
+            fmt (str): The input format of the variant. Must be either "hgvs" or "spdi".
+
+        Raises:
+            ValueError: If an unsupported format is provided (i.e., not "hgvs" or "spdi").
+
+        Returns:
+            object: A FHIR Variation object representing the parsed variant.
+        """
         if fmt == "hgvs":
             return self._from_hgvs(var)
         elif fmt == "spdi":
