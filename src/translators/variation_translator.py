3ad45ff44e317ac75542705061ef0e20cd120394
SalemBajjali <salem.bajjali@gmail.com>
2025-11-25 13:09:33 -0600

    feat: add coordinate interval methods for SPDI and HGVS formats in VariationTranslation

--- src/translators/variation_translator.py
+++ src/translators/variation_translator.py
@@ -20,13 +20,92 @@ from resources.moleculardefinition import (
 from translators.allele_utils import detect_sequence_type
 from vrs_tools.hgvs_tools import HgvsToolsLite
 
-
 class VariationTranslation:
 
     def __init__(self, dp=None, uri: str | None = None):
         self.dp = dp or create_dataproxy(uri=uri)
         #most likely need to replace this
         self.hgvs_tools = HgvsToolsLite(data_proxy=self.dp)
+
+    def _spdi_coordinate_interval(self):
+        system = CodeableConcept(
+                coding=[Coding(
+                        system="http://loinc.org",
+                        code =  "LA30100-4",
+                        display =  "0-based interval counting",
+                )
+                ]
+            )
+        
+        origin = CodeableConcept(
+                coding=[Coding(
+                        system="http://hl7.org/fhir/uv/molecular-definition-data-types/CodeSystem/coordinate-origin",
+                        code =  "sequence-start",
+                        display =  "Sequence start",
+                )
+                ]
+            )
+
+        normalizationMethod = CodeableConcept(
+                coding=[Coding(
+                        system="http://hl7.org/fhir/uv/molecular-definition-data-types/CodeSystem/normalization-method",
+                        code =  "fully-justified", 
+                        display = "Fully justified" 
+                )
+                ]
+            )
+        
+        return system, origin, normalizationMethod
+    
+    def _hgvs_coordinate_interval(self):
+        system = CodeableConcept(
+                coding=[Coding(
+                        system="http://loinc.org",
+                        code =  "LA30102-0",
+                        display =  "1-based character counting",
+                )
+                ]
+            )
+        
+        origin = CodeableConcept(
+                coding=[Coding(
+                        system="http://hl7.org/fhir/uv/molecular-definition-data-types/CodeSystem/coordinate-origin",
+                        code =  "feature-start",
+                        display =  "Feature start",
+                )
+                ]
+            )
+
+        normalizationMethod = CodeableConcept(
+                coding=[Coding(
+                        system="http://hl7.org/fhir/uv/molecular-definition-data-types/CodeSystem/normalization-method",
+                        code =  "right-shift", 
+                        display = "Right shift" 
+                )
+                ]
+            )
+        
+        return system, origin, normalizationMethod
+    
+    def _hgvs_position(self, sv):
+        pos = sv.posedit.pos
+        return pos.start.base, pos.end.base
+    
+    # TODO: This function was copied from another module. Consider moving it to `allele_utils.py`.
+    # NOTE: The name `allele_utils` might not be ideal, since this function is also used in the
+    #       variation module, which could cause confusion.
+    def _refseq_to_fhir_id(self, refseq_accession):
+        """Converts a RefSeq accession string to a standardized FHIR ID format.
+        This method removes the version suffix (after the dot), strips out underscores,
+        and converts the string to lowercase to ensure compatibility with FHIR resource IDs.
+
+        Args:
+            refseq_accession (str): A RefSeq accession string (e.g., 'NM_001256789.1').
+
+        Returns:
+            str: A formatted FHIR-compatible ID (e.g., 'nm001256789').
+        """
+        return refseq_accession.split(".", 1)[0].replace("_", "").lower()
 
     def _from_spdi(self,spdi):
         """Parse an SPDI string and convert it into a FHIR Variation Profile object.
@@ -74,46 +153,35 @@ class VariationTranslation:
             "alt_seq": alt_seq
         }
 
-        return self._create_variation_profile(values)
+        return self._create_variation_profile(values,fmt="spdi")
 
     def _from_hgvs(self,hgvs_expr):
-        """Parse an HGVS string and convert it into a FHIR Variation Profile object  
-
-        Args:
-            hgvs_expr (str): A valid hgvs expression. 
-
-        Raises:
-            ValueError: If the HGVS expression represents an intronic variant, which is not supported.
-            NotImplementedError: If the HGVS edit type is not supported by this implementation.
-
-        Returns:
-            object: A FHIR Variation Profile object representing the parsed HGVS variant.
-        """
-        # we need to parse
+        
         sv = self.hgvs_tools.parse(hgvs_expr)
         if not sv:
             return None
 
-        # need to check if its intronic
         if self.hgvs_tools.is_intronic(sv):
             raise ValueError("Intronic HGVS variants are not supported")
 
-        start, end, alt_seq = self.hgvs_tools.get_position_and_state(sv)
+        start_pos, end_pos, alt_seq = self.hgvs_tools.get_position_and_state(sv)
 
         edit_type = self.hgvs_tools.get_edit_type(sv)
 
         if edit_type in {"del","delins","dup"}:
-            ref_seq = self.dp.get_sequence(sv.ac,start,end)
+            ref_seq = self.dp.get_sequence(sv.ac,start_pos,end_pos)
         elif edit_type == "ins":
             ref_seq = sv.posedit.edit.ref or ""
         elif edit_type == "sub":
             ref_seq = sv.posedit.edit.ref
         elif edit_type == "identity":
-            ref_seq = self.dp.get_sequence(sv.ac,start,end)
+            ref_seq = self.dp.get_sequence(sv.ac,start_pos,end_pos)
             if not ref_seq:
                 ref_seq = sv.posedit.edit.alt or ""
         else:
             raise NotImplementedError(f"Unsupported HGVS edit type: {edit_type}")
+        
+        start,end = self._hgvs_position(sv)
 
         values = {
             "refget_accession": sv.ac,
@@ -123,24 +191,9 @@ class VariationTranslation:
             "alt_seq": alt_seq,
             }
 
-        return self._create_variation_profile(values)
-    # TODO: This function was copied from another module. Consider moving it to `allele_utils.py`.
-    # NOTE: The name `allele_utils` might not be ideal, since this function is also used in the
-    #       variation module, which could cause confusion.
-    def _refseq_to_fhir_id(self, refseq_accession):
-        """Converts a RefSeq accession string to a standardized FHIR ID format.
-        This method removes the version suffix (after the dot), strips out underscores,
-        and converts the string to lowercase to ensure compatibility with FHIR resource IDs.
+        return self._create_variation_profile(values, fmt="hgvs")
 
-        Args:
-            refseq_accession (str): A RefSeq accession string (e.g., 'NM_001256789.1').
-
-        Returns:
-            str: A formatted FHIR-compatible ID (e.g., 'nm001256789').
-        """
-        return refseq_accession.split(".", 1)[0].replace("_", "").lower()
-
-    def _create_variation_profile(self,values):
+    def _create_variation_profile(self,values,fmt):
         """Create a FHIR Variation resource from parsed variant data (HGVS or SPDI).
 
         Args:
@@ -161,23 +214,10 @@ class VariationTranslation:
                 display =  f"{sequence_type} Sequence",
                 )])
 
-        coord_system_values = CodeableConcept(
-                coding=[Coding(
-                        system="http://loinc.org",
-                        code =  "LA30100-4",
-                        display =  "0-based interval counting",
-                )
-                ]
-            )
-
-        # normalization_method = CodeableConcept(
-        #         coding=[Coding(
-        #                 system="http://hl7.org/fhir/uv/molecular-definition-data-types/CodeSystem/normalization-method",
-        #                 code =  "left-shift",
-        #                 display = "Left shift"
-        #         )
-        #         ]
-        #     )
+        if fmt == "hgvs":
+            coord_system_values, coord_system_origin, normalization_method = self._hgvs_coordinate_interval()
+        elif fmt == "spdi":
+            coord_system_values, coord_system_origin, normalization_method = self._spdi_coordinate_interval()
 
         fhir_id = self._refseq_to_fhir_id(refseq_accession=values["refget_accession"])
 
@@ -189,7 +229,8 @@ class VariationTranslation:
 
         coord_system = MolecularDefinitionLocationSequenceLocationCoordinateIntervalCoordinateSystem(
                 system=coord_system_values,
-                # normalizationMethod= normalization_method
+                origin=coord_system_origin,
+                normalizationMethod= normalization_method
             )
 
         start, end = Quantity(value=int(values["start"])),Quantity(value=int(values["end"]))
@@ -263,8 +304,3 @@ class VariationTranslation:
             return self._from_spdi(var)
         else:
             raise ValueError("Only 'hgvs' and 'spdi' formats are supported.")
-
-
-
-
-
